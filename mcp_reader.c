e22b7e42e2a9703edf161f43961875bbf5f1dc0b
boblaublaw <github@sublimation.org>
2015-03-03 21:13:07 -0800

    moving reader code into the reader module

---
+++ mcp_reader.c
@@ -0,0 +1,36 @@
+#include <stdio.h>      // fopen
+#include <assert.h>     // assert
+#include <string.h>     // bzero, strerrno
+#include <errno.h>      // errno
+
+#include "mcp.h"
+
+int createReader(mcp_reader_t *mr, char *filename)
+{
+    bzero(mr, sizeof(mcp_reader_t));
+    if (NULL == (mr->source = fopen(filename,"r"))) {
+        fprintf(stderr, "Could not open %s: %s\n", filename, strerror(errno));
+        return -1;
+    }
+
+    // determine the size of the input file
+    fseek(mr->source, 0L, SEEK_END);
+    mr->size = ftell(mr->source);
+    fseek(mr->source, 0L, SEEK_SET);
+
+    return 0;
+}
+
+int startReader(mcp_reader_t *mr)
+{
+    assert(mr);
+    assert(mr->source);
+
+
+
+    fclose(mr->source);
+
+    return 0;
+}
+
+
