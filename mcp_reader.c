ac32b61041c8f1492cc3bc4612e61466dd6d1add
boblaublaw <github@sublimation.org>
2015-06-08 14:56:54 -0700

    refactored reader to use a buffer manager

--- mcp_reader.c
+++ mcp_reader.c
@@ -51,6 +51,60 @@ int initReader(mcp_reader_t *mr, const char *filename, int writerCount, int hash
     return 0;
 }
 
+//  return codes:
+//                  -1 = something went wrong, exit before done
+//                  0  = read all the data, exit normally
+//                  1  = more data to read, keep going
+int readIntoBuf(mcp_reader_t *mr, int bufId) 
+{
+    assert(mr);
+    assert(mr->source);
+    int retval;
+
+    logDebug("reader about to read into BUF %d\n", bufId);
+
+    if (-1 == (mr->bufBytes[bufId]  = fread(mr->buf[bufId], 1, PAGESIZE, mr->source))) {
+        logError("failed to read from %s: %s\n", mr->filename, strerror(errno));
+        exitFlag = 1;
+        return -1;
+    }
+
+    if (mr->hashFiles && mr->bufBytes[bufId])
+        CC_MD5_Update(&mr->md5state, mr->buf[bufId], mr->bufBytes[bufId]);
+
+    logDebug("reader read %ld bytes into BUF %d\n", mr->bufBytes[bufId], bufId);
+    logDebug("reader about to wait for BUF %d barrier\n", bufId);
+
+    if (-1 == (retval = pthread_barrier_waitcancel(&mr->barrier[bufId], &exitFlag))) {
+        if (!exitFlag) {
+            logDebug("reader failed to wait for BUF %d barrier\n", bufId);
+        }
+        exitFlag=1;
+        return -1;
+    }
+
+    if (retval == ETIMEDOUT) {
+        if (!exitFlag) {
+            logDebug("reader timed out on BUF %d\n", bufId);
+        }
+        exitFlag=1;
+        return -1;
+    }
+
+    logDebug("reader done waiting for BUF %d barrier\n", bufId);
+        
+    if (exitFlag) {
+        logDebug("reader told to shut down by another thread\n");
+        return 0;
+    }
+
+    if (mr->bufBytes[bufId] == 0) {
+        logDebug("reader ran out of data reading into BUF %d\n", bufId);
+        return 0;
+    }
+    return 1;
+}
+
 void *startReader(void *arg) 
 
 {
@@ -59,113 +113,14 @@ void *startReader(void *arg)
     assert(mr->source);
     
     long retval = 0;
+    int bufId = 0;
 
     while(1) {
-
-        logDebug2("reader reached loop begin\n");
-
-        // ========================= A BUFFER READ, B BUFFER WRITE START====================
-
-        logDebug("reader about to read into BUF A\n");
-
-        if (-1 == (mr->bufBytes[BUF_A]  = fread(mr->buf[BUF_A], 1, PAGESIZE, mr->source))) {
-            logError("failed to read from %s: %s\n", mr->filename, strerror(errno));
-            retval = EXIT_FAILURE;
-            exitFlag = 1;
-            goto pthread_exit;
+        if (1 != (retval = readIntoBuf(mr, bufId))) {
+                goto pthread_exit;
         }
-
-        if (mr->hashFiles && mr->bufBytes[BUF_A])
-            CC_MD5_Update(&mr->md5state, mr->buf[BUF_A], mr->bufBytes[BUF_A]);
-
-        logDebug("reader read %ld bytes into BUF A\n", mr->bufBytes[BUF_A]);
-
-        // ========================= A BUFFER READ, B BUFFER WRITE END======================
-
-        //  make sure all the writers are done writing before we advance any further
-        logDebug("reader about to wait for BUF A barrier\n");
-
-        if (-1 == (retval = pthread_barrier_waitcancel(&mr->barrier[BUF_A], &exitFlag))) {
-            if (!exitFlag) {
-                logDebug("reader failed to wait for BUF A barrier\n");
-                retval = EXIT_FAILURE;
-            }
-            exitFlag=1;
-            goto pthread_exit;
-        }
-
-        if (retval == ETIMEDOUT) {
-            if (!exitFlag) {
-                logDebug("reader timed out on BUF A\n");
-            }
-            retval = EXIT_FAILURE;
-            exitFlag=1;
-            goto pthread_exit;
-        }
-
-        logDebug("reader done waiting for BUF A barrier\n");
-        
-        if (exitFlag) {
-            logDebug("reader told to shut down\n");
-            break;
-        }
-
-        if (mr->bufBytes[BUF_A] == 0) {
-            logDebug("reader ran out of data in BUF A\n");
-            break;
-        }
-
-        // ========================= B BUFFER READ, A BUFFER WRITE START====================
-       
-        logDebug("reader about to read into BUF B\n");
-
-        if (-1 == (mr->bufBytes[BUF_B]  = fread(mr->buf[BUF_B], 1, PAGESIZE, mr->source))) {
-            logError("failed to read from %s: %s\n", mr->filename, strerror(errno));
-            retval = EXIT_FAILURE;
-            exitFlag = 1;
-            goto pthread_exit;
-        }
-
-        if (mr->hashFiles && mr->bufBytes[BUF_B])
-            CC_MD5_Update(&mr->md5state, mr->buf[BUF_B], mr->bufBytes[BUF_B]);
-
-        logDebug("reader read %ld bytes into BUF B\n", mr->bufBytes[BUF_A]);
-
-        // ========================= B BUFFER READ, A BUFFER WRITE END======================
-        
-        logDebug("reader about to wait for BUF B barrier\n");
-            
-        if (-1 == (retval = pthread_barrier_waitcancel(&mr->barrier[BUF_B], &exitFlag))) {
-            if (!exitFlag) {
-                logDebug("reader failed to wait for BUF B barrier\n");
-                retval = EXIT_FAILURE;
-            }
-            exitFlag=1;
-            goto pthread_exit;
-        }
-
-        if (retval == ETIMEDOUT) {
-            if (!exitFlag) {
-                logDebug("reader timed out on BUF B\n");
-            }
-            retval = EXIT_FAILURE;
-            exitFlag=1;
-            goto pthread_exit;
-        }
-
-        logDebug("reader done waiting for BUF B barrier\n");
-
-        if (exitFlag) {
-            logDebug("reader told to shut down\n");
-            break;
-        }
-
-        if (mr->bufBytes[BUF_B] == 0) {
-            logDebug("reader ran out of data in BUF B\n");
-            break;
-        }
-
-        logDebug2("reader reached loop end\n");
+        bufId = !bufId;
+        logDebug("setting new bufId to %d\n", bufId);
     }
 
     logDebug("reader closing file\n");
@@ -178,6 +133,7 @@ pthread_exit:
     logDebug("reader closed file\n");
 
     if (!exitFlag && mr->hashFiles) {
+        logDebug("reader hashing file\n");
         int i;
         CC_MD5_Final(mr->md5sum, &mr->md5state);
         logDebug("md5 state is: ");
